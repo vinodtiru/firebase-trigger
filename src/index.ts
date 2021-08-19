f03f500623046aed78688f62dd05dde7c4338206
Vinod <vinodtiru@gmail.com>
2021-08-19 15:31:44 +0530

    5

--- src/index.ts
+++ src/index.ts
@@ -86,9 +86,11 @@ const processAction = () => {
       age: 3,
     };
 
+    const fol = new DocFileSystem("vinod","icon");
+
     // write path to Firestore
     // updateFirestoreDatabase(projName + "-docs", "path", new DocFileSystem("vinod","icon"));
-    updateFirestoreDatabase(projName + "-docs", "path", value2);
+    updateFirestoreDatabase(projName + "-docs", "path", fol);
     core.info(`Data written to DB`);
 
     // setLastUpdatedTimeToDB();
